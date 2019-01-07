3e8cd7abd23fb3d31ff19ef96a8e681ee265778b
Song Zheng <song@zheng.club>
2019-01-07 00:21:36 -0800

    private route

---
+++ src/components/PrivateRoute.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Route, Redirect } from 'react-router-dom'
+
+const PrivateRoute = ({
+  setRedirectPath,
+  component: Component,
+  ...otherProps
+}) => {
+  const renderRoute = props =>
+    window.userInfo.auth ? <Component {...props} /> : <Redirect to='/signin' />
+  return <Route {...otherProps} render={renderRoute} />
+}
+
+export default PrivateRoute
