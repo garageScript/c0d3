35ff43d6040b95e22dd59317defc5eca5e4926da
Song Zheng <song@zheng.club>
2019-01-06 23:28:02 -0800

    public page

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+import * as serviceWorker from './serviceWorker'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { loadUserInfo } from './helpers/auth/actions'
+import chatdb from './db/chatdb'
+
+loadUserInfo(() => {
+  const userInfo = { ...window.userInfo }
+  document.getElementById('root').style.display = 'block'
+
+  if (userInfo && userInfo.id) {
+    chatdb.startSocket(userInfo)
+  }
+
+  if ((!userInfo || !userInfo.userName) && window.location.pathname === '/') {
+    return
+  }
+
+  ReactDOM.render(<App />, document.getElementById('root'))
+})
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.unregister()
