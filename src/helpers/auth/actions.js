55117adfaf3cc3d58faf0db5ea1cc15b2b4e705f
Song Zheng <song@zheng.club>
2019-01-06 23:52:59 -0800

    auth files and signin signup files

---
+++ src/helpers/auth/actions.js
@@ -0,0 +1,38 @@
+import client from './client'
+import { garageScript as defaultUserInfo } from './defaultUserInfo'
+
+const { session, account, validator } = client
+const location = window.location
+
+export const loadUserInfo = callback => {
+  session.getInfo(({ userInfo }) => {
+    console.log('loading info...')
+    window.userInfo = userInfo
+      ? { ...defaultUserInfo, ...userInfo }
+      : { ...defaultUserInfo }
+
+    callback()
+  })
+}
+
+export const signIn = (username, password) => {
+  session.start(username, password, ({ userInfo }) => {
+    if (userInfo) location.pathName = window.userInfo.redirectPath
+  })
+}
+
+export const signOut = () => {
+  session.end(() => location.reload())
+}
+
+export const signUp = formData => {
+  account.create(formData, ({ success }) => {
+    if (success) signIn(formData.username, formData.password)
+  })
+}
+
+export const validate = validator
+
+export const setRedirectPath = path => {
+  window.userInfo.redirectPath = path
+}
---
+++ src/helpers/auth/defaultUserInfo.js
@@ -0,0 +1,31 @@
+export const garageScript = {
+  auth: false,
+  id: '',
+  name: '',
+  email: '',
+  userName: '',
+  signUpSuccess: false,
+  signUpFeedback: '',
+  redirectPath: '/curriculum'
+};
+
+export const gsEmail = {
+  auth: false,
+  fullName: '',
+  userName: '',
+  m8lEmail: '',
+  email: '',
+  id: '',
+  userId: '',
+  companyId: '',
+  domain: '',
+  role: '',
+  status: '',
+  trialStart: '',
+  paymentInfo: {
+    active: false,
+    planType: ''
+  },
+  message: '',
+  error: ''
+};
