bde3213bd6147bd3eec97f9e10f9f5103b3c5f24
Tri Le <trile86@gmail.com>
2019-03-01 16:21:02 -0800

    production ready submit command

--- gsSubmit/cmd/submit/index.js
+++ gsSubmit/cmd/submit/index.js
@@ -1,27 +1,19 @@
 const chalk = require('chalk')
+const { URL } = require('url')
 const git = require('simple-git')()
 const prompt = require('prompt')
 const { request } = require('graphql-request')
 const credService = require('../util/credentials.js')
 
-let diff
-let userId
-let lessons
-let lessonsByOrder
-let lessonOrder
-let lessonId
-let challengesByOrder
-let challengeOrder
-let challengeId
-
 module.exports = async (inputs) => {
 
   try {
     const credentials = await credService.getCredentials()
-    const url = inputs.url ? `${inputs.url}/signin` : 'https://c0d3.com/signin'
+    const url = inputs.url ?
+      new URL('/signin', inputs.url) : new URL('/signin', 'https://c0d3.com')
 
     if (!credentials.token) {
-      const success = await credService.validate(credentials, url)
+      const success = await credService.validate(credentials, url.href)
       if (!success) return console.error('Invalid Credentials')
       credService.save(credentials)
     }
@@ -29,179 +21,29 @@ module.exports = async (inputs) => {
     const currentBranch = await checkCurrentBranch()
     await getDiffAgainstMaster(currentBranch)
 
-    const graphqlEndpoint = getGraphqlEndpoint(inputs)
+    const graphqlEndpoint = getGraphqlEndpoint(inputs).href
+    if (process.env.TEST && (inputs.username || inputs.u)) {
+      credentials.username = inputs.username || inputs.u
+    }
     const userId = await getUserId(credentials.username, graphqlEndpoint)
+    const lessons = await queryForLessons(graphqlEndpoint)
+    const currentLesson = await promptForLessons(lessons)
+    const challengeId = await promptForChallenge(currentLesson)
+    const diff = await createDiff(currentBranch)
 
-    return new Promise((resolve, reject) => {
-      })
-  .then(() => {
-    // Query for the lessons
-    const lessonsQuery = `
-  {
-    lessons {
-       id
-       title
-       challenges {
-         id
-         title
-         order
-       }
-       order
-     }
-  }`
-    return request(graphqlEndpoint, lessonsQuery).then(res => {
-      console.log('got lessons: ', lessons)
-      lessons = res.lessons
-    })
-  })
-  .then(() => {
-    // Prompt for the lessons we want
-    lessonsByOrder = lessons
-      .sort((a, b) => a.order - b.order)
-      .reduce((acc, { order, id, title, challenges }) => {
-        console.log(`Enter ${order} to submit to lesson: ${title}. `)
-        acc[order] = {
-          title,
-          challenges,
-          id
-        }
-        return acc
-      }, {})
+    // From this point onward, all graphql calls are mutating
+    // database data.
+    await enrollStudent(currentLesson.id, userId, graphqlEndpoint)
+    await sendSubmission(currentLesson.id, userId, diff, challengeId, graphqlEndpoint)
 
-    const schema = {
-      properties: {
-        lessonOrder: {
-          pattern: /^[0-9][0-9]*$/,
-          message:
-            'The lesson number needs to be a non-negative integer.\n' +
-            'To cancel submission, press Ctrl + d',
-          required: true
-        }
-      }
+    } catch(e) {
+      console.error(e)
     }
-
-    prompt.start()
-    return new Promise((resolve, reject) => {
-      prompt.get(schema, function (err, result) {
-        lessonOrder = result.lessonOrder
-        console.log('Command-line input received:')
-        console.log('  Lesson Number: ' + lessonOrder)
-        if (err) return reject(err)
-        const currentLesson = lessonsByOrder[lessonOrder]
-        lessonId = currentLesson.id
-        if (!currentLesson) {
-          return reject('The lesson number you gave does not exist')
-        }
-        return resolve(currentLesson)
-      })
-    })
-  })
-  .then(currentLesson => {
-    // Prompt for the challenge we want
-    challengesByOrder = currentLesson.challenges
-      .sort((a, b) => a.order - b.order)
-      .reduce((acc, { title, order, id }) => {
-        console.log(`Enter ${order} to submit to challenge: ${title}. `)
-        acc[order] = {
-          title,
-          id
-        }
-        return acc
-      }, {})
-
-    const schema = {
-      properties: {
-        challengeOrder: {
-          pattern: /^[0-9][0-9]*$/,
-          message:
-            'The challenge number needs to be a non-negative integer.\n' +
-            'To cancel submission, press Ctrl + d',
-          required: true
-        }
-      }
-    }
-
-    prompt.start()
-    return new Promise((resolve, reject) => {
-      prompt.get(schema, function (err, result) {
-        challengeOrder = result.challengeOrder
-        console.log('Command-line input received:')
-        console.log('  Challenge Number: ' + challengeOrder)
-        if (err) return reject(err)
-        const currentChallenge = challengesByOrder[challengeOrder]
-        if (!currentChallenge) {
-          return reject(
-            'The challenge assigned to the number did not exist'
-          )
-        }
-        challengeId = currentChallenge.id
-        return resolve()
-      })
-    })
-  })
-  .then(() => {
-    // Create the diff that is markdown compatible
-    return new Promise((resolve, reject) => {
-      git.diff([`master..${current}`], (error, stdout, stderr) => {
-        if (error || stderr) return reject(error || stderr)
-        diff = stdout
-        return resolve()
-      })
-    })
-  })
-  // From this point onward, all graphql calls are mutating
-  // database data.
-  .then(() => {
-    // Enroll a student
-    const enrollMutation = `
-    mutation enrollStudent($input: LessonId) {
-      enrollStudent(input: $input) {
-        isPassed
-        isTeaching
-        isEnrolled
-      }
-    }`
-    const enrollVariables = {
-      input: {
-        id: lessonId,
-        userId: `${userId}`
-      }
-    }
-    return request(graphqlEndpoint, enrollMutation, enrollVariables)
-  })
-  .then(() => {
-    // Send a submission to the graphql endpoint
-    const createSubmission = `
-    mutation CreateSubmission($submission: SubmissionInput) {
-      createSubmission(input: $submission) {
-        id
-        diff
-        order
-      }
-    }`
-
-    const variablesForCreation = {
-      submission: {
-        lessonId,
-        challengeId,
-        userId: `${userId}`,
-        diff
-      }
-    }
-    return request(graphqlEndpoint, createSubmission, variablesForCreation)
-  })
-  .then(() => {
-    console.log('\nYour submission was successfully received')
-  })
-  .catch(console.log)
-  } catch(e) {
-    console.error(e)
-  }
 }
 
 function getGraphqlEndpoint(inputs) {
-  if (process.env.TEST && inputs.url) return `${inputs.url}/graphql`
-  return 'https://c0d3.com/graphql'
+  if (inputs.url) return new URL('/graphql', inputs.url)
+  return new URL('/graphql', 'https://c0d3.com')
 }
 
 function checkCurrentBranch() {
@@ -244,11 +86,161 @@ function getUserId(username, graphqlEndpoint) {
       username,
     }
   }
+
   return request(graphqlEndpoint, usersQuery, usersQueryVar)
     .then(({ users }) => {
-      console.log('got users', users)
       const [user] = users
-      userId = user.id
+      return user.id
       }
     )
+}
+
+function queryForLessons(graphqlEndpoint) {
+  const lessonsQuery = `
+  {
+    lessons {
+      id
+      title
+      challenges {
+        id
+        title
+        order
+      }
+      order
+    }
+  }`
+  return request(graphqlEndpoint, lessonsQuery).then(res => res.lessons)
+}
+
+function promptForLessons(lessons) {
+  const lessonsByOrder = lessons
+    .sort((a, b) => a.order - b.order)
+    .reduce((acc, { order, id, title, challenges }) => {
+      console.log(`Enter ${order} to submit to lesson: ${title}. `)
+      acc[order] = {
+        title,
+        challenges,
+        id
+      }
+      return acc
+    }, {})
+
+  const schema = {
+    properties: {
+      lessonOrder: {
+        pattern: /^[0-9][0-9]*$/,
+        message:
+          'The lesson number needs to be a non-negative integer.\n' +
+          'To cancel submission, press Ctrl + d',
+        required: true
+      }
+    }
+  }
+
+  prompt.message = ''
+  prompt.start()
+  return new Promise((resolve, reject) => {
+    prompt.get(schema, function (err, result) {
+      console.log('Command-line input received:')
+      console.log('  Lesson Number: ' + result.lessonOrder)
+      if (err) return reject(err)
+      const currentLesson = lessonsByOrder[result.lessonOrder]
+      if (!currentLesson) {
+        return reject('The lesson number you gave does not exist')
+      }
+      return resolve(currentLesson)
+    })
+  })
+}
+
+function promptForChallenge(currentLesson) {
+  const challengesByOrder = currentLesson.challenges
+    .sort((a, b) => a.order - b.order)
+    .reduce((acc, { title, order, id }) => {
+      console.log(`Enter ${order} to submit to challenge: ${title}. `)
+      acc[order] = {
+        title,
+        id
+      }
+      return acc
+    }, {})
+
+  const schema = {
+    properties: {
+      challengeOrder: {
+        pattern: /^[0-9][0-9]*$/,
+        message:
+          'The challenge number needs to be a non-negative integer.\n' +
+          'To cancel submission, press Ctrl + d',
+        required: true
+      }
+    }
+  }
+
+  prompt.message = ''
+  prompt.start()
+  return new Promise((resolve, reject) => {
+    prompt.get(schema, function (err, result) {
+      console.log('Command-line input received:')
+      console.log('  Challenge Number: ' + result.challengeOrder)
+      if (err) return reject(err)
+      const currentChallenge = challengesByOrder[result.challengeOrder]
+      if (!currentChallenge) {
+        return reject(
+          'The challenge assigned to the number did not exist'
+        )
+      }
+      return resolve(currentChallenge.id)
+    })
+  })
+}
+
+function createDiff(currentBranch) {
+  return new Promise((resolve, reject) => {
+    git.diff([`master..${currentBranch}`], (error, stdout, stderr) => {
+      if (error || stderr) return reject(error || stderr)
+      return resolve(stdout)
+    })
+  })
+}
+
+function enrollStudent(lessonId, userId, graphqlEndpoint) {
+  const enrollMutation = `
+  mutation enrollStudent($input: LessonId) {
+    enrollStudent(input: $input) {
+      isPassed
+      isTeaching
+      isEnrolled
+    }
+  }`
+  const enrollVariables = {
+    input: {
+      id: lessonId,
+      userId: `${userId}`
+    }
+  }
+  return request(graphqlEndpoint, enrollMutation, enrollVariables)
+}
+
+function sendSubmission(lessonId, userId, diff, challengeId, graphqlEndpoint) {
+  const createSubmission = `
+  mutation CreateSubmission($submission: SubmissionInput) {
+    createSubmission(input: $submission) {
+      id
+      diff
+      order
+    }
+  }`
+
+  const variablesForCreation = {
+    submission: {
+      lessonId,
+      challengeId,
+      userId: `${userId}`,
+      diff
+    }
+  }
+
+  return request(graphqlEndpoint, createSubmission,variablesForCreation)
+    .then(() => console.log('\nYour submission was successfully received'))
 }
