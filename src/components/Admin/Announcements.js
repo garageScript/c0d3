e5dd91d75d2c467cdb10d55ae8c37e2a20b76664
Rahul Kalra <noob101@garagescript.org>
2019-03-06 16:30:55 -0800

    Imporved CSS for Announcement Page

--- src/components/Admin/Announcements.js
+++ src/components/Admin/Announcements.js
@@ -32,58 +32,69 @@ class Announcements extends React.Component {
   render () {
     return (
       <div>
-        <h1>Admin Page</h1>
-        <h1>New Announcements</h1>
-        <textarea className='newAnnouncement' value={this.state.announcement} onChange={(v) => {
-          this.setState({
-            announcement: v.target.value
-          })
-        }} />
-        <Mutation update={cacheUpdate(GET_ANNOUNCEMENTS, ({ createAnnouncement }, { announcements }) => {
-          return {
-            announcements: [createAnnouncement].concat(announcements)
-          }
-        })} mutation={CREATE_ANNOUNCEMENT}>
-          {(execute) => {
-            return <button className='updateAnnouncement' onClick={() => {
-              execute({
-                variables: {
-                  input: this.state.announcement
-                }
-              })
-              this.setState({
-                announcement: ''
-              })
-            }}>SUBMIT</button>
-          }}
-        </Mutation>
-        <Query query={GET_ANNOUNCEMENTS} >
-          { loadComponent(({ announcements }) => {
-            return announcements.map((v, i) => {
-              return (
-                <div>
-                  <Markdown key={i} source={v.description} />
-                  <Mutation update={cacheUpdate(GET_ANNOUNCEMENTS, ({ deleteAnnouncement }) => {
-                    return {
-                      announcements: deleteAnnouncement
-                    }
-                  })} mutation={DELETE_ANNOUNCEMENT}>
-                    {(execute) => {
-                      return <button className='deleteAnnouncement' onClick={() => {
-                        execute({
-                          variables: {
-                            input: v.id
-                          }
-                        })
-                      }}>DELETE</button>
-                    }}
-                  </Mutation>
-                  <hr />
-                </div>
-              )
+        <h1 style={{ textAlign: 'center' }}>Admin Page</h1>
+        <h4 style={{
+          marginTop: '80px',
+          marginLeft: '143px'
+        }}>New Announcements: </h4>
+        <div className='container' style={{ display: 'flex' }}>
+          <textarea className='newAnnouncement' style={{
+            width: '100%',
+            height: '100px'
+          }} value={this.state.announcement} onChange={(v) => {
+            this.setState({
+              announcement: v.target.value
             })
-          })}
-        </Query>
+          }} />
+          <Mutation update={cacheUpdate(GET_ANNOUNCEMENTS, ({ createAnnouncement }, { announcements }) => {
+            return {
+              announcements: [createAnnouncement].concat(announcements)
+            }
+          })} mutation={CREATE_ANNOUNCEMENT}>
+            {(execute) => {
+              return <button className='updateAnnouncement' onClick={() => {
+                execute({
+                  variables: {
+                    input: this.state.announcement
+                  }
+                })
+                this.setState({
+                  announcement: ''
+                })
+              }}>SUBMIT</button>
+            }}
+          </Mutation>
+        </div>
+        <div className='container'>
+          <Query query={GET_ANNOUNCEMENTS} >
+            { loadComponent(({ announcements }) => {
+              return announcements.map((v, i) => {
+                return (
+                  <div>
+                    <Markdown key={i} source={v.description} />
+                    <Mutation update={cacheUpdate(GET_ANNOUNCEMENTS, ({ deleteAnnouncement }) => {
+                      return {
+                        announcements: deleteAnnouncement
+                      }
+                    })} mutation={DELETE_ANNOUNCEMENT}>
+                      {(execute) => {
+                        return <button className='deleteAnnouncement' onClick={() => {
+                          execute({
+                            variables: {
+                              input: v.id
+                            }
+                          })
+                        }}>DELETE</button>
+                      }}
+                    </Mutation>
+                    <hr />
+                  </div>
+                )
+              })
+            })}
+          </Query>
+
+        </div>
       </div>
     )
   }
