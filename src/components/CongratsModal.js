f8407240d31008811c69ac69bd8ddf80d0045438
Peter Chon <PeteC@garagescript.org>
2019-03-15 16:26:32 -0700

    congratsModal made into a class component with all dependent variables changed to class format.  At saving comment stage

--- src/components/CongratsModal.js
+++ src/components/CongratsModal.js
@@ -60,14 +60,104 @@ class CongratsModal extends React.Component {
   constructor ({ lessonInfo }) {
     super({ lessonInfo })
     this.state = {
+      lessonInfo,
       commentUpdate: null,
       selected: {}
     }
-
   }
 
   render () {
-    return <h1> test  </h1>
+    return (
+      <Query query={LESSON_STATUS} variables={{ in: { id: this.state.lessonInfo.id } }}>
+        {({ loading, error, data }) => {
+          /* if (error || loading) return ''
+          if (!data || !data.lessonStatus) return ''
+          if (
+            !data.lessonStatus.isTeaching ||
+          !data.lessonStatus.isPassed ||
+          data.lessonStatus.starGiven
+          ) { return '' } */
+
+          return (
+            <div
+              className='modal fade show'
+              id='basicExampleModal'
+              tabIndex='-1'
+              role='dialog'
+              aria-labelledby='exampleModalLabel'
+              style={{ display: 'block', backgroundColor: 'rgba(0,0,0,0.8)' }}
+            >
+              <Query query={STAR_RECIPIENT}>
+                {clientState => {
+                  return (
+                    <div className='modal-dialog' role='document'>
+                      <div className='modal-content'>
+                        <div className='modal-header'>
+                          <h5 className='modal-title' id='exampleModalLabel'>
+                          Congratulation on passing {this.state.lessonInfo.title}
+                          </h5>
+                        </div>
+                        <div
+                          className='modal-body'
+                          style={{ height: '200px', overflow: 'auto' }}
+                        >
+                          <h5>Who helped you the most?</h5>
+                          <Teachers
+                            clientState={clientState}
+                            lessonInfo={this.state.lessonInfo}
+                            selected={this.selected}
+                            selectUser={uid => {
+                              this.setState({
+                                selected: { userId: uid }
+                              }, () => { console.log('state updated:', this.state) })
+                            }}
+                          />
+                        </div>
+                        <div className='modal-footer'>
+                          <Mutation mutation={GIVE_STAR}>
+                            {execute => {
+                              const starRecipent =
+                              clientState.data.starRecipent || 'no one'
+                              return (
+                                <div>
+                                  <textarea />
+                                  <button
+                                    type='button'
+                                    className='btn btn-default btn-lg btn-block'
+                                    data-dismiss='modal'
+                                    aria-label='Close'
+                                    onClick={() => {
+                                      execute({
+                                        variables: {
+                                          in: {
+                                            lessonId: this.state.lessonInfo.id,
+                                            userId: this.state.selected.userId,
+                                            comment: 'good job!'
+                                          }
+                                        }
+                                      }).then(() => window.location.reload())
+                                    // TODO: close the modal in a more classy way
+                                    }}
+                                  >
+                                    <span aria-hidden='true'>
+                                  Give Star to {starRecipent}
+                                    </span>
+                                  </button>
+                                </div>
+                              )
+                            }}
+                          </Mutation>
+                        </div>
+                      </div>
+                    </div>
+                  )
+                }}
+              </Query>
+            </div>
+          )
+        }}
+      </Query>
+    )
   }
 }
 
