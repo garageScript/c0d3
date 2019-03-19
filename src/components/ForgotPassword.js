d1e3430161273d8212f5a0e8d1567f4bf515b0ef
Rahul Kalra <noob101@garagescript.org>
2019-03-19 14:48:31 -0700

    updates fix #108 with best practice use of () when returning components

--- src/components/ForgotPassword.js
+++ src/components/ForgotPassword.js
@@ -1,14 +1,16 @@
 import React from 'react'
 
 const ForgotPassword = () => {
-  return <div className='container'>
-    <form className='auth-form'>
-      <p className='h5 mb-4'> Forgot Password?</p>
-      <p>Please enter your email address below to recieve the reset password link.</p>
+  return (
+    <div className='container'>
+      <form className='auth-form'>
+        <p className='h5 mb-4'> Forgot Password?</p>
+        <p>Please enter your email address below to recieve the reset password link.</p>
       Email: <input />
-      <button className='btn btn-primary'>Submit</button>
-    </form>
-  </div>
+        <button className='btn btn-primary'>Submit</button>
+      </form>
+    </div>
+  )
 }
 
 export default ForgotPassword
