1f8f77974a5f19aab87a3a00e0b9c4a4987d4b72
Peter Chon <PeteC@garagescript.org>
2019-03-19 14:41:21 -0700

    fix #96 CongratsModal is shown when the User has not given a star

--- src/components/CongratsModal.js
+++ src/components/CongratsModal.js
@@ -64,7 +64,7 @@ class CongratsModal extends React.Component {
              !lessonStatus ||
             !lessonStatus.isTeaching ||
             !lessonStatus.isPassed ||
-            !lessonStatus.starGiven
+            lessonStatus.starGiven
           ) { return '' }
 
         return (
