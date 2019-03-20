5416fec05a7ae92b46628d22de4704612e6ee98d
Rahul Kalra <noob101@garagescript.org>
2019-03-20 10:26:21 -0700

    updates Fixes #108 with BEST CODING PRACTICES!

--- src/components/ForgotPassword.js
+++ src/components/ForgotPassword.js
@@ -1,18 +1,17 @@
 import React from 'react'
 
 const ForgotPassword = () => {
+  let emailRef = React.reactRef()
   return (
     <div className='container'>
       <form className='auth-form'>
         <p className='h5 mb-4'> Forgot Password?</p>
         <p>Please enter your email address below to recieve the reset password link.</p>
-        <label htmlFor='email'>
+        <label htmlFor='forgotPasswordEmail'>
       Email: <input
         id='email'
         placeholder='hello@c0d3.com'
-        onChange={(event) => {
-              const emailAddress = event.target.value
-            }}
+        ref={emailRef}
           />
         </label>
         <button className='btn btn-primary'>Submit</button>
