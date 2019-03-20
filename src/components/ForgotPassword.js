c561bd0a13eaea7ea65ea66da9f580a76d2d6e74
Rahul Kalra <noob101@garagescript.org>
2019-03-19 18:53:53 -0700

    updates fix #108 with better form structure for the ForgotPassword form

--- src/components/ForgotPassword.js
+++ src/components/ForgotPassword.js
@@ -6,7 +6,15 @@ const ForgotPassword = () => {
       <form className='auth-form'>
         <p className='h5 mb-4'> Forgot Password?</p>
         <p>Please enter your email address below to recieve the reset password link.</p>
-      Email: <input />
+        <label htmlFor='email'>
+      Email: <input
+        id='email'
+        placeholder='hello@c0d3.com'
+        onChange={(event) => {
+              const emailAddress = event.target.value
+            }}
+          />
+        </label>
         <button className='btn btn-primary'>Submit</button>
       </form>
     </div>
