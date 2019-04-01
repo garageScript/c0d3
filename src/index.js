1fd433dc86f4259e8303c3894a6e2ff1987a8b02
Alberto Lopez <albertolopez7@gmail.com>
2019-04-01 10:12:54 -0700

    delete font awesome in src/index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom'
 import './css/index.css'
 import App from './App'
 import * as serviceWorker from './serviceWorker'
-import '@fortawesome/fontawesome-free/css/all.min.css'
 
 import { loadUserInfo } from './helpers/auth/actions'
 import chatdb from './db/chatdb'
