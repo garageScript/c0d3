619ef77fdb33ea36e8418f7244fe3f99d994c2c0
Alberto Lopez <albertolopez7@gmail.com>
2019-04-03 15:41:26 -0700

    added nanoid and the ability to update token if it does not exist

--- Server/dbload.js
+++ Server/dbload.js
@@ -122,7 +122,7 @@ Message.belongsTo(Room)
 Message.belongsTo(User)
 Room.hasMany(Message)
 
-// sequelize.sync({ alter: true })
+sequelize.sync({ alter: true })
 
 module.exports = {
   Announcement,
