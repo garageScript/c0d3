5183c07e4b80a6144753ec7849b2af0a8f9bce0b
song zheng <song@zheng.club>
2019-08-05 09:23:51 -0700

    Remove chat - closes #198

--- Server/graphql/index.js
+++ Server/graphql/index.js
@@ -1,18 +1,5 @@
 const { makeExecutableSchema, mergeSchemas } = require('graphql-tools')
 
-// Schema for chat
-const chatTypeDefs = require('../chat/graphql/typeDefs')
-const chatQueries = require('../chat/graphql/queries')
-const chatMutations = require('../chat/graphql/mutations')
-const chatResolvers = {
-  Query: chatQueries,
-  Mutation: chatMutations
-}
-const chatSchema = makeExecutableSchema({
-  typeDefs: chatTypeDefs,
-  resolvers: chatResolvers
-})
-
 // Schema for curriculum
 const curriculumTypeDefs = require('./typedefs')
 const curriculumQueries = require('./queries')
@@ -40,5 +27,5 @@ const linkTypeDefs = `
 `
 
 module.exports = mergeSchemas({
-  schemas: [curriculumSchema, chatSchema, linkTypeDefs]
+  schemas: [curriculumSchema, linkTypeDefs]
 })
--- src/index.js
+++ src/index.js
@@ -5,17 +5,12 @@ import App from './App'
 import * as serviceWorker from './serviceWorker'
 
 import { loadUserInfo } from './helpers/auth/actions'
-import chatdb from './db/chatdb'
 
 loadUserInfo(() => {
   const userInfo = { ...window.userInfo }
 
   // TODO: Remove hack. Replace all instances of userName with username
   userInfo.userName = userInfo.username
-
-  if (userInfo && userInfo.id) {
-    chatdb.startSocket(userInfo)
-  }
 
   if ((!userInfo || !userInfo.userName) && window.location.pathname === '/') {
     return
